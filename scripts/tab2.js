8730e1a20fed0fa43bc01582ac5a0aaba19d4033
Jubakee <jubakee@gamil.com>
2024-08-03 20:27:51 +1200

    Omega

--- scripts/tab2.js
+++ scripts/tab2.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Predefined items for the shop
     const items = [
-        { id: '1', name: 'WOODEN CHEST', price: 100, img: 'assets/chest.png', description: 'A Wooden Chest', rarity: 'Common', level: '1', isOpen: false },
-        { id: '2', name: 'SILVER CHEST', price: 200, img: 'assets/chest.png', description: 'A Silver Chest', rarity: 'Uncommon', level: '2', isOpen: false },
-        { id: '3', name: 'GOLD CHEST', price: 300, img: 'assets/chest.png', description: 'A Gold Chest', rarity: 'Rare', level: '3', isOpen: false },
-        { id: '4', name: 'DIAMOND CHEST', price: 400, img: 'assets/chest.png', description: 'A Diamond Chest', rarity: 'Unique', level: '4', isOpen: false },
+        { id: '1', name: 'WOODEN CHEST', price: 100, img: 'assets/chest_wooden.png', description: 'WOODEN CHEST', rarity: 'Common', level: '1', isOpen: false },
+        { id: '2', name: 'SILVER CHEST', price: 200, img: 'assets/chest_silver.png', description: 'SILVER CHEST', rarity: 'Uncommon', level: '2', isOpen: false },
+        { id: '3', name: 'GOLD CHEST', price: 300, img: 'assets/chest_gold.png', description: 'GOLDEN CHEST', rarity: 'Rare', level: '3', isOpen: false },
+        { id: '4', name: 'DIAMOND CHEST', price: 400, img: 'assets/chest_diamond.png', description: 'A DIAMOND CHEST', rarity: 'Unique', level: '4', isOpen: false },
     ];
 
     // Render shop items
@@ -20,9 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>
             <img src="${item.img}" alt="${item.name}">
             <div class="item-info">
-                <div>
                     <div class="item-description">${item.description}</div>
-                </div>
                 <div class="item-price">💵 ${item.price}</div>
             </div>
         `;
